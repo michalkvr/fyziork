030a6185ce5c727c0f0f95990937a1eb6ce3c22f
Michal Kovář <michalkvr.dev@gmail.comgit config --global user.name Michal>
2022-03-06 14:13:15 +0100

    [ADD] Sections, gallery, footer, a bit of responsivity

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 1,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+
+});
